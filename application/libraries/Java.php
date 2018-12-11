fb904219b192002f01a50e292d3f50e688e795e7
suchartbu <suchartbu@gmail.com>
2018-12-11 18:55:40 +0700

    Update putenv()

--- application/libraries/Java.php
+++ application/libraries/Java.php
@@ -8,10 +8,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class Java {
 
-    public function Exec($file_name) {
-        $file_name = '/var/www/html/OrrCodeIgniter_3/java/'.$file_name . '.jar';
-        $output = NULL;
+    public function __construct() {
         putenv('LANG=en_US.UTF-8');
+    }
+
+    public function Exec($file_name) {
+        $file_name = '/var/www/html/OrrCodeIgniter_3/java/' . $file_name . '.jar';
+        $output = NULL;
+
         exec('java -jar ' . $file_name, $output);
         return $output;
     }
