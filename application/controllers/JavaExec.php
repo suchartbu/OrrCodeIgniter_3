3e29cb402537b0ca1e89ed4cc8e4a6d51260b095
suchartbu <suchartbu@gmail.com>
2018-12-12 13:07:34 +0700

    Add JavaExec

---
+++ application/controllers/JavaExec.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * ตัวอย่างการใช้โปรแกรมจาวาเพื่อจัดการข้อมูล
+ * @author suchart bunhachirat
+ */
+class JavaExec extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('java');
+    }
+
+    public function index(){
+        $out = $this->java->Exec('HelloWorldApp');
+        echo'ตัวอย่างข้อมูลจากโปรแกรมจาวา  ';
+        print_r($out);
+        $this->load->view('JavaExec_');
+    }
+}
