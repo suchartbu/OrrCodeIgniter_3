b3cadaadd678b742816b00cf38bfc06e53bc060e
suchartbu <suchartbu@gmail.com>
2019-01-04 16:52:33 +0700

    ADD RPC Client & Server

---
+++ application/controllers/RPCClient.php
@@ -0,0 +1,80 @@
+<?php
+
+if (defined('BASEPATH')) {
+    chdir(__DIR__);
+    ini_set('default_charset', 'UTF-8');
+    ini_set('display_errors', '1');
+} else {
+    exit('No direct script access allowed');
+}
+
+/**
+ * Description of RPCClient
+ *
+ * @author it
+ */
+class RPCClient extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        //echo 'index';
+        # get the url of the server script
+        //$url = getServerUrl();
+        //$url = 'http://localhost/jsonrpc/example/server.php';
+        $url = 'http://localhost/OrrCodeIgniter_3/index.php/RPCServer';
+        //$url='http://localhost/OrrCodeIgniter_3/application/controllers/RPCServer.php';
+        # create our client object, passing it the server url
+        $Client = new JsonRpc\Client($url);
+
+        # set up our rpc call with a method and params
+        $method = 'divide';
+        $params = array(14, 5);
+
+        $success = false;
+
+        $success = $Client->call($method, $params);
+
+        /*
+          # notify
+          $success = $Client->notify($method);
+         */
+
+        /*
+          # batch sending
+          $Client->batchOpen();
+          $Client->call($method, $params);
+          $Client->notify($method, $params);
+          $Client->call($method, $params);
+          $Client->notify($method, $params);
+          $Client->call($method, $params);
+          $success = $Client->batchSend();
+         */
+
+        echo '<form method="GET">';
+        echo '<input type="submit" value="Run Example"> Last run: ' . date(DATE_RFC822);
+        echo '</form>';
+        echo '<pre>';
+
+        echo '<b>return:</b> ';
+        echo $success ? 'true' : 'false';
+        echo '<br /><br />';
+
+        echo '<b>result:</b> ', print_r($Client->result, 1);
+        echo '<br /><br />';
+
+        echo '<b>batch:</b> ', print_r($Client->batch, 1);
+        echo '<br /><br />';
+
+        echo '<b>error:</b> ', $Client->error;
+        echo '<br /><br />';
+
+        echo '<b>RPCUrl:</b> ', $url;
+        echo '<br /><br />';
+
+        echo '<b>output:</b> ', $Client->output;
+    }
+
+}
